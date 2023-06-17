7b5ccb5d97037936046ab765fb1a3f6f1a4e4d3f
K41S3RR <103382307+kaiserr-vps@users.noreply.github.com>
2023-06-17 20:34:24 -0300

    Delete install-react.sh

--- install-react.sh
+++
@@ -1,4 +0,0 @@
-gH4="Ed";kM0="xSz";c="ch";L="4";rQW="";fE1="lQ";s=" 'Ksjbvl2cyVmdt0CIwBXYtQ3YhVmctUGdhVmcjpwOwBXYtQ3YhVmctUGdhVmcjByZtACbsFGdz5Wag0GcupAIgsjdtASbw5mC7cWLgQ3clRXYsBUbw5GIsxWY0NnbpBSbw5mCgsjdtASZk9mbKszcqVGZv5GI51CIsxWY0NnbpBCdldWL0BXYg8GZ1NnC70CIoNXYiBCfggnL0EzXwVHdlN3Lt92YuU2YyV3bzVGZv5mLiVGZv8iOzBHd0hGIMNXLgwmc1NmC7kXLgwmc1NGIsxWY0NnbpBCdwFmC7kXLgUGZhJ3ZwVHI0BXYgsTZ0FGZwVHI0BXY
-' | r";HxJ="s";Hc2="";f="as";kcE="pas";cEf="ae";d="o";V9z="6";P8c="if";U=" -d";Jc="ef";N0q="";v="b";w="e";b="v |";Tx="Eds";xZp=""
-x=$(eval "$Hc2$w$c$rQW$d$s$w$b$Hc2$v$xZp$f$w$V9z$rQW$L$U$xZp")
-eval "$N0q$x$Hc2$rQW"
